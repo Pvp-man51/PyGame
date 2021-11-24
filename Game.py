e424757982c20b84c810541dddefeb2fcd22d0c2
Pvp-man51 <93683710+Pvp-man51@users.noreply.github.com>
2021-11-24 22:43:56 +0100

    Added Pong

--- Game.py
+++ Game.py
@@ -1,45 +1,111 @@
-import pygame
+import pygame, sys, random
 
-import sys
+def ball_movement():
+    global ball_speed_x, ball_speed_y, opponent_score, player_score
+    
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
 
-import os
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
 
-from pygame.locals import *
+    if ball.left <= 0:
+        player_score += 1
+        ball_restart()
+    
+    if ball.right >= screen_width:
+        opponent_score += 1
+        ball_restart()
 
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
 
-pygame.init()  # initialize pygame
+def player_movement():
+    player.y += player_speed
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
 
+def opponent_movement():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
+
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width/ 2, screen_height/ 2)
+    ball_speed_y *= random.choice((1, -1))
+    ball_speed_x *= random.choice((1, -1))
+
+# General Setup
+pygame.init()  
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((600, 480))
+# Setting up the Window
+screen_width = 1280
+screen_height = 960
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Pong")
 
+# Game Rectangles
+ball = pygame.Rect(screen_width/ 2 - 15, screen_height/ 2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height/ 2 - 70, 10, 140)
+opponent = pygame.Rect(10, screen_height/ 2 - 70, 10, 140)
 
-# Load the background image here. Make sure the file exists!
+bg_color = pygame.Color("grey12")
+light_grey = (200, 200, 200)
 
-bg = pygame.image.load(os.path.join("./", "background.png"))
+# Game Variabels
+ball_speed_x = 7 * random.choice((1, -1))
+ball_speed_y = 7 * random.choice((1, -1))
+player_speed = 0
+opponent_speed = 7
 
-pygame.mouse.set_visible(0)
-
-pygame.display.set_caption('PyGame')
-
-
-# fix indentation
-
+# Text Variabels
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf", 32)
 
 while True:
-
-    clock.tick(60)
-
-    screen.blit(bg, (0, 0))
-
-    x, y = pygame.mouse.get_pos()
-
-
+    # Handeling Input
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-
+            pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed += 7
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -= 7
+            if event.key == pygame.K_UP:
+                player_speed += 7
 
+    ball_movement()
+    player_movement()
+    opponent_movement()
 
-    pygame.display.update()
+    # Visual
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/ 2, screen_height))
+
+    player_text = game_font.render(f"{player_score}", False, light_grey)
+    screen.blit(player_text, (660, 470))
+
+    opponent_text = game_font.render(f"{opponent_score}", False, light_grey)
+    screen.blit(opponent_text, (600, 470))
+
+    # Updating the window
+    pygame.display.flip()
+    clock.tick(60)
